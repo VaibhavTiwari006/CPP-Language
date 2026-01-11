7d641972d7c709fe43dc27e882b07e73b4dc7195
Vaibhav <vaibhav.cse006@gmail.com>
2026-01-11 10:17:13 +0530

    Add area of square code

---
+++ Basics/AreaOfSquare.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s;
+    cout << "Enter the side of square\n";
+    cin >> s;
+    cout << "Area of square is "<<  s*s;
+    return 0;
+}
