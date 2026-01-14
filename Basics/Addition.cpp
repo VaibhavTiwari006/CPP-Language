f83e5259faa397e16993b6b497838dcbd613e55f
Vaibhav <vaibhav.cse006@gmail.com>
2026-01-14 09:46:30 +0530

    Add Addition code

---
+++ Basics/Addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter your first number\n";
+
+    cin >> a;
+    cout << "Enter your second number\n";
+
+    cin >> b;
+    cout << "The sum of a and b is \n";
+
+    cout << (a + b);
+}
