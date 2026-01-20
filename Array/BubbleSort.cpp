aa4b2de68bee05f7e186bd39e61fb61c2100e26f
Vaibhav <vaibhav.cse006@gmail.com>
2026-01-20 15:17:43 +0530

    Add bubble sort program

---
+++ Array/BubbleSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,temp;
+    cout << "enter number of elements" << endl;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    //bubble sort
+    for (int i = 0; i < n-1; i++) {
+        for(int j =0 ; j < n-i-1 ; j++){
+        if(arr[j] > arr[j+1]){
+            temp = arr[j+1];
+            arr[j+1] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    }
+     for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
