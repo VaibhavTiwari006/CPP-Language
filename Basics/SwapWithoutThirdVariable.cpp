f488a2d9c1ad02f1a33f8a8d96713f2ad577cb00
Vaibhav <vaibhav.cse006@gmail.com>
2026-02-07 15:51:16 +0530

    Add swaping two numbers without third variable

---
+++ Basics/SwapWithoutThirdVariable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    // Swapping without temporary variable using XOR
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    cout << "After swapping:" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    return 0;
+}
