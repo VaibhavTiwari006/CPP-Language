b59db0f741030644acc15c68f261bfc8a9c36472
Vaibhav <vaibhav.cse006@gmail.com>
2026-02-18 01:02:36 +0530

    Add Bubble sort code

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int n,temp;
-    cout << "enter number of elements" << endl;
+int main(){
+    int n ,temp;
+    cout << "Enter number of elements in array" << endl;
     cin >> n;
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i] ;
     }
-    //bubble sort
-    for (int i = 0; i < n-1; i++) {
-        for(int j =0 ; j < n-i-1 ; j++){
-        if(arr[j] > arr[j+1]){
-            temp = arr[j+1];
-            arr[j+1] = arr[j];
-            arr[j] = temp;
+    for(int i = 0 ; i < n ; i++){
+        int flag = 0;
+        for(int j = 0 ; j < n-1-i ; j++){ // -i because the last element is sorted with each loop
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                flag = 1;
+            }
+        }
+        if(flag == 0){ // to avoid checking again if array is already sorted
+            break;
         }
     }
-    }
-     for (int i = 0; i < n; i++) {
+    cout << "The sorted array is :" << endl;
+    for(int i = 0 ; i < n ; i++){
         cout << arr[i] << " ";
     }
 }
